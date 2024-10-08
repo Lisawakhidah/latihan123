4fbc0ab8d896ec8e702ce2207f2228b2916ef2dd
Lisawakhidah <wakhidahlisa@gmail.com>
2024-10-08 17:14:34 +0700

    belajar javascript dasar

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+//alert("heyho")
+
+// prompt('masukkan usia anda?')
+
+//variabel sebuah wadah untuk menampung inputan user, nilai bisa juga
+// var usia = 22
+//  const usia = 22//tidak bisa diubah
+// let usia = 2 // masih bisa di ubah
+// usia = 35
+// console.log("haloo usia kamu adalah " + usia)
+// alert ("usia kamu adalah " + usia)
+let usia = prompt("berapa usia kamu?")
+alert ("usia kamu adalah " + usia)
