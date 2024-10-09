7d9c995a73ae7a6977039820ce8a9e06a30a2978
Lisawakhidah <wakhidahlisa@gmail.com>
2024-10-09 13:35:55 +0700

    array js

---
+++ javascript/array.js
@@ -0,0 +1,7 @@
+// let namaGuru = ["jodi", "jehay", "Rahman"]
+// namaGuru.push("Lisa", "fikri")//nambahkan array
+// namaGuru.shift()//menghapus awal array
+// namaGuru.pop()//menghapus akhir
+
+// alert(namaGuru)
+
