032ddd0e5735715006b221de10b2b9fc45777eb3
Lisawakhidah <wakhidahlisa@gmail.com>
2024-10-09 14:31:01 +0700

    looping

---
+++ javascript/looping.js
@@ -0,0 +1,28 @@
+//for loop
+//3 statements 
+
+// const namaGuru = ['dea','fikri','fitri','yusril','falah']
+// console.log(namaGuru.length)
+
+// for(let i = 0; i<namaGuru.length; i++){
+//     console.log(namaGuru[i])
+// }
+
+// const buah =['apel','mangga','jeruk']
+// console.log(buah.length)
+
+// for(let i = 0;i<buah.length; i++){
+//     console.log(buah[i])
+// }
+
+//do while
+let i=0
+do{
+    i++
+    console.log("dea afrizal")
+}while (i < 10)
+// while (i<10){
+//     i++
+//     console.log('dea afrizal')
+// }
+
